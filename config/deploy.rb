45d69e1e612575b9515e04177fef1e63a6d41216
Shaan <shardendu.kumar@venturepact.com>
2015-12-21 13:44:12 +0530

    changes ind eploy shared dir

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :deploy_to, '/home/deploy/code-analyzer'
 set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system storage}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
