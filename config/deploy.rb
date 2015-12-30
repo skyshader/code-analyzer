5e2a79ec0542bb458709c774ee35465cb5ce7f84
Shaan <shardendu.kumar@venturepact.com>
2015-12-30 15:57:59 +0530

    changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.1.0'
 
 set :application, 'code-analyzer'
-set :repo_url, 'git@github.com-shardenduvp:skyshader/code-analyzer.git'
+set :repo_url, 'git@github.com:skyshader/code-analyzer.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
