c58298c005a97602c8b6408e8ba19a2717118b97
Shaan <shardendu.kumar@venturepact.com>
2016-01-04 14:42:31 +0530

    checking log

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -38,7 +38,8 @@ class MainController < ApplicationController
 
     # activate key by requesting github
     def ssh_key_activate username
-      ssh_cmd = 'ssh -T git@github.com-' + username
+      ssh_cmd = "ssh -T git@github.com-" + username
+      puts ssh_cmd
       result = `#{ssh_cmd}`
       logger.debug "SSH Activate - " + result.to_s
     end
