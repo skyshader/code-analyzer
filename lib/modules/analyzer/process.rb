bfae0770035d0003ec77115f277849d5ea8cbd05
Shardendu <shardendukumargautam@gmail.com>
2016-02-24 17:25:53 +0530

    added few modules and classes

---
+++ lib/modules/analyzer/process.rb
@@ -0,0 +1,14 @@
+# Analyzer -> process.rb
+#
+# This class will be used to do initial processing
+# like detecting languages used in repo
+# and generating list of files to be processed
+# with their respective engines 
+
+module Analyzer
+  class Process
+  
+    
+
+  end
+end
---
+++ lib/modules/engines/phpcs_engine.rb
@@ -0,0 +1,11 @@
+# Analyzer -> phpcs_engine.rb
+#
+
+
+module Engines
+  class PHPcsEngine
+  
+    
+
+  end
+end
