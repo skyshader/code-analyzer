27b09bf0822c599bf8147de195821bc8512eea7e
Shaan <shardendu.kumar@venturepact.com>
2016-03-07 15:30:19 +0530

    added partitioned code issues table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+# IssueCategory -> seed
+issue_category_data = [
+  { name: 'style', description: 'The coding standard issues in your code.' },
+  { name: 'complexity', description: 'The complexity of your code.' },
+  { name: 'clarity', description: 'The clarity and understandability of your code.' },
+  { name: 'compatibility', description: 'The compatibility of your code within modules.' },
+  { name: 'security', description: 'The security vulnerabilities in your code.' },
+  { name: 'bug risk', description: 'The possible bug risks in your code.' }
+]
+
+IssueCategory.create(issue_category_data)
--- lib/modules/analyzer/engines/php/mess_detector/config.rb
+++ lib/modules/analyzer/engines/php/mess_detector/config.rb
@@ -17,50 +17,50 @@ module Analyzer
 
           CATEGORIES = {
             # clean code rules
-            "BooleanArgumentFlag" => ["Clarity", 300000],
-            "ElseExpression" => ["Clarity", 200000],
-            "StaticAccess" => ["Clarity", 200000],
+            "BooleanArgumentFlag" => ["clarity", 300000],
+            "ElseExpression" => ["clarity", 200000],
+            "StaticAccess" => ["clarity", 200000],
 
             # code size rules
-            "CyclomaticComplexity" => ["Complexity", 100000],
-            "NpathComplexity" => ["Complexity", 200000],
-            "ExcessiveMethodLength" => ["Complexity", 200000],
-            "ExcessiveClassLength" => ["Complexity", 200000],
-            "ExcessiveParameterList" => ["Complexity", 200000],
-            "ExcessivePublicCount" => ["Complexity", 700000],
-            "TooManyFields" => ["Complexity", 900000],
-            "TooManyMethods" => ["Complexity", 2000000],
-            "ExcessiveClassComplexity" => ["Complexity", 200000],
+            "CyclomaticComplexity" => ["complexity", 100000],
+            "NpathComplexity" => ["complexity", 200000],
+            "ExcessiveMethodLength" => ["complexity", 200000],
+            "ExcessiveClassLength" => ["complexity", 200000],
+            "ExcessiveParameterList" => ["complexity", 200000],
+            "ExcessivePublicCount" => ["complexity", 700000],
+            "TooManyFields" => ["complexity", 900000],
+            "TooManyMethods" => ["complexity", 2000000],
+            "ExcessiveClassComplexity" => ["complexity", 200000],
 
             # controversial rules
-            "Superglobals" => ["Security", 100000],
-            "CamelCaseClassName" => ["Style", 500000],
-            "CamelCasePropertyName" => ["Style", 500000],
-            "CamelCaseMethodName" => ["Style", 1000],
-            "CamelCaseParameterName" => ["Style", 500000],
-            "CamelCaseVariableName" => ["Style", 25000],
+            "Superglobals" => ["security", 100000],
+            "CamelCaseClassName" => ["style", 500000],
+            "CamelCasePropertyName" => ["style", 500000],
+            "CamelCaseMethodName" => ["style", 1000],
+            "CamelCaseParameterName" => ["style", 500000],
+            "CamelCaseVariableName" => ["style", 25000],
 
             # design rules
-            "ExitExpression" => ["Bug Risk", 200000],
-            "EvalExpression" => ["Security", 300000],
-            "GotoStatement" => ["Clarity", 200000],
-            "NumberOfChildren" => ["Clarity", 1000000],
-            "DepthOfInheritance" => ["Clarity", 500000],
-            "CouplingBetweenObjects" => ["Clarity", 400000],
+            "ExitExpression" => ["bug risk", 200000],
+            "EvalExpression" => ["security", 300000],
+            "GotoStatement" => ["clarity", 200000],
+            "NumberOfChildren" => ["clarity", 1000000],
+            "DepthOfInheritance" => ["clarity", 500000],
+            "CouplingBetweenObjects" => ["clarity", 400000],
             
             # naming rules
-            "ShortVariable" => ["Style", 500000],
-            "LongMethod" => ["Complexity", 200000],
-            "ShortMethodName" => ["Style", 800000],
-            "ConstructorWithNameAsEnclosingClass" => ["Compatibility", 400000],
-            "ConstantNamingConventions" => ["Style", 100000],
-            "BooleanGetMethodName" => ["Style", 200000],
+            "ShortVariable" => ["style", 500000],
+            "LongMethod" => ["complexity", 200000],
+            "ShortMethodName" => ["style", 800000],
+            "ConstructorWithNameAsEnclosingClass" => ["compatibility", 400000],
+            "ConstantNamingConventions" => ["style", 100000],
+            "BooleanGetMethodName" => ["style", 200000],
             
             # unused code rules
-            "UnusedPrivateField" => ["Bug Risk", 200000],
-            "UnusedLocalVariable" => ["Bug Risk", 200000],
-            "UnusedPrivateMethod" => ["Bug Risk", 200000],
-            "UnusedFormalParameter" => ["Bug Risk", 200000],
+            "UnusedPrivateField" => ["bug risk", 200000],
+            "UnusedLocalVariable" => ["bug risk", 200000],
+            "UnusedPrivateMethod" => ["bug risk", 200000],
+            "UnusedFormalParameter" => ["bug risk", 200000],
           }.freeze
 
         end
