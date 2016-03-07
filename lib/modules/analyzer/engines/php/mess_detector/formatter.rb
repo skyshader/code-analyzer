bbd94efcbd2daf4c15b9f1b3060956ef174692f9
Vijayant Soni <vijayant.soni@venturepact.com>
2016-03-07 15:33:01 +0530

    added xml parser for phpmd result

--- lib/modules/analyzer/engines/php/mess_detector/formatter.rb
+++ lib/modules/analyzer/engines/php/mess_detector/formatter.rb
@@ -2,6 +2,9 @@
 #
 # To format/parse the output given from the engine
 
+require 'rubygems'
+require 'nokogiri'
+
 module Analyzer
   module Engines
     module PHP
@@ -10,11 +13,28 @@ module Analyzer
         class Formatter
 
           def self.xml_to_hash xml
-            xml
+            xml_doc = Nokogiri::XML(xml)
+            file_array = xml_doc.xpath("//file")
+            errors = []
+            file_array.each do |file|
+              file.elements.each do |error|
+                errors << {
+                  engine: "phpmd",
+                  file_path: file["name"],
+                  begin_line: error["beginline"],
+                  end_line: error["endline"],
+                  issue_column: nil,
+                  severity: nil,
+                  issue_text: error.inner_text,
+                  source: error["rule"],
+                  priority: error["priority"]
+                }
+              end
+            end
+            errors
           end
 
         end
-
       end
     end
   end
