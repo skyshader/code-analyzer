e83c2f701d775012bb6678824fdcb1abd82543bb
Vijayant Soni <vijayant.soni@venturepact.com>
2016-03-07 16:06:04 +0530

    added parser for phpmd result

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,135 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151208122539) do
+ActiveRecord::Schema.define(version: 20160307092102) do
 
+  create_table "branches", force: :cascade do |t|
+    t.string   "name",                  limit: 255
+    t.integer  "is_analyzed",           limit: 4,   default: 0
+    t.integer  "is_activity_generated", limit: 4,   default: 0
+    t.integer  "status",                limit: 4,   default: 1
+    t.integer  "repository_id",         limit: 4
+    t.datetime "created_at",                                    null: false
+    t.datetime "updated_at",                                    null: false
+  end
+
+  add_index "branches", ["repository_id"], name: "index_branches_on_repository_id", using: :btree
+
+  create_table "code_issues", id: false, force: :cascade do |t|
+    t.integer  "id",                limit: 8,     default: 0, null: false
+    t.text     "file_path",         limit: 65535
+    t.text     "issue_text",        limit: 65535
+    t.integer  "begin_line",        limit: 4
+    t.integer  "end_line",          limit: 4
+    t.integer  "issue_column",      limit: 4
+    t.text     "source_code",       limit: 65535
+    t.integer  "weight",            limit: 4
+    t.string   "engine",            limit: 255
+    t.string   "engine_ruleset",    limit: 255
+    t.integer  "branch_id",         limit: 4,     default: 0, null: false
+    t.integer  "issue_category_id", limit: 4
+    t.integer  "file_list_id",      limit: 4
+    t.datetime "created_at",                                  null: false
+    t.datetime "updated_at",                                  null: false
+  end
+
+  add_index "code_issues", ["branch_id"], name: "index_code_issues_on_branch_id", using: :btree
+  add_index "code_issues", ["file_list_id"], name: "index_code_issues_on_file_list_id", using: :btree
+  add_index "code_issues", ["issue_category_id"], name: "index_code_issues_on_issue_category_id", using: :btree
+
+  create_table "commits", force: :cascade do |t|
+    t.string   "sha",            limit: 255
+    t.date     "date"
+    t.datetime "full_date"
+    t.integer  "additions",      limit: 4
+    t.integer  "deletions",      limit: 4
+    t.integer  "files_changed",  limit: 4
+    t.integer  "contributor_id", limit: 4
+    t.integer  "branch_id",      limit: 4
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
+
+  add_index "commits", ["branch_id"], name: "index_commits_on_branch_id", using: :btree
+  add_index "commits", ["contributor_id"], name: "index_commits_on_contributor_id", using: :btree
+
+  create_table "contributors", force: :cascade do |t|
+    t.string   "name",          limit: 255
+    t.string   "email",         limit: 255
+    t.integer  "additions",     limit: 4
+    t.integer  "deletions",     limit: 4
+    t.integer  "total_commits", limit: 4
+    t.integer  "branch_id",     limit: 4
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
+
+  add_index "contributors", ["branch_id"], name: "index_contributors_on_branch_id", using: :btree
+
+  create_table "file_lists", force: :cascade do |t|
+    t.string   "name",          limit: 255
+    t.integer  "is_file",       limit: 4
+    t.string   "extension",     limit: 255
+    t.string   "language",      limit: 255
+    t.integer  "file_size",     limit: 4
+    t.string   "phash",         limit: 255
+    t.string   "fhash",         limit: 255
+    t.string   "relative_path", limit: 255
+    t.string   "parent_path",   limit: 255
+    t.text     "full_path",     limit: 65535
+    t.integer  "is_excluded",   limit: 4,     default: 0
+    t.integer  "status",        limit: 4,     default: 1
+    t.integer  "branch_id",     limit: 4
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
+  end
+
+  add_index "file_lists", ["branch_id"], name: "index_file_lists_on_branch_id", using: :btree
+
+  create_table "issue_categories", force: :cascade do |t|
+    t.string   "name",        limit: 255
+    t.text     "description", limit: 65535
+    t.integer  "status",      limit: 4,     default: 1
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
+  end
+
+  create_table "repositories", force: :cascade do |t|
+    t.string   "username",       limit: 255
+    t.string   "owner",          limit: 255
+    t.string   "name",           limit: 255
+    t.string   "ssh_url",        limit: 255
+    t.text     "directory",      limit: 65535
+    t.string   "default_branch", limit: 255
+    t.string   "current_branch", limit: 255
+    t.integer  "is_private",     limit: 4
+    t.integer  "is_setup",       limit: 4,     default: 0
+    t.integer  "project_id",     limit: 4,                 null: false
+    t.integer  "status",         limit: 4
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
+  end
+
+  add_index "repositories", ["project_id"], name: "index_repositories_on_project_id", using: :btree
+
+  create_table "request_logs", force: :cascade do |t|
+    t.string   "process_type",   limit: 255
+    t.string   "request_type",   limit: 255
+    t.integer  "success_status", limit: 4,     default: 0
+    t.integer  "is_error",       limit: 4,     default: 0
+    t.integer  "error_status",   limit: 4,     default: 0
+    t.text     "error_message",  limit: 65535
+    t.integer  "branch_id",      limit: 4
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
+  end
+
+  add_index "request_logs", ["branch_id"], name: "index_request_logs_on_branch_id", using: :btree
+
+  add_foreign_key "branches", "repositories"
+  add_foreign_key "commits", "branches"
+  add_foreign_key "commits", "contributors"
+  add_foreign_key "contributors", "branches"
+  add_foreign_key "file_lists", "branches"
+  add_foreign_key "request_logs", "branches"
 end
--- lib/modules/analyzer/engines/php/mess_detector/formatter.rb
+++ lib/modules/analyzer/engines/php/mess_detector/formatter.rb
@@ -12,6 +12,7 @@ module Analyzer
 
         class Formatter
 
+
           def self.xml_to_hash xml
             xml_doc = Nokogiri::XML(xml)
             file_array = xml_doc.xpath("//file")
@@ -19,19 +20,24 @@ module Analyzer
             file_array.each do |file|
               file.elements.each do |error|
                 errors << {
-                  engine: "phpmd",
                   file_path: file["name"],
+                  issue_text: error.inner_text,
                   begin_line: error["beginline"],
                   end_line: error["endline"],
                   issue_column: nil,
-                  severity: nil,
-                  issue_text: error.inner_text,
-                  source: error["rule"],
-                  priority: error["priority"]
+                  source_code: ,                  
+                  weight: ,
+                  engine: "phpmd",
+                  engine_ruleset: ,
+                  issue_category: get_issue_category_id(error["rule"])
                 }
               end
             end
             errors
+          end
+
+          def get_issue_category_id(rule)
+            
           end
 
         end
