8af12ac0f5d60e84cc72003197121bebef7e8ec4
Vijayant Soni <vijayant.soni@venturepact.com>
2016-03-07 17:02:17 +0530

    parser added for phpmd

--- lib/modules/analyzer/engines/php/mess_detector/formatter.rb
+++ lib/modules/analyzer/engines/php/mess_detector/formatter.rb
@@ -2,9 +2,6 @@
 #
 # To format/parse the output given from the engine
 
-require 'rubygems'
-require 'nokogiri'
-
 module Analyzer
   module Engines
     module PHP
@@ -25,22 +22,47 @@ module Analyzer
                   begin_line: error["beginline"],
                   end_line: error["endline"],
                   issue_column: nil,
-                  source_code: ,                  
-                  weight: ,
+                  source_code: get_source_code(
+                    file["name"], [
+                      error["beginline"],
+                      error["endline"]
+                    ]
+                  ),
+                  weight: get_rule_weight(error["rule"]),
                   engine: "phpmd",
-                  engine_ruleset: ,
-                  issue_category: get_issue_category_id(error["rule"])
+                  engine_ruleset: error["rule"],
+                  issue_category_id: get_issue_category(error["rule"]).id || default_category.id
                 }
               end
             end
             errors
           end
 
-          def get_issue_category_id(rule)
-            
+          def get_issue_category(rule)
+            Analyzer::Engines::PHP::MessDetector::Config::CATEGORIES.each do |category, data|
+              if category == rule
+                return IssueCategory.find_by(name: data.first)                
+              end
+            end
           end
 
-        end
+          def default_category
+            IssueCategory.find_by(name: "style")
+          end
+
+          def get_rule_weight(rule)
+            Analyzer::Engines::PHP::MessDetector::Config::CATEGORIES.each do |category, data|
+              if category == rule
+                return data.last                
+              end
+          end
+
+          def get_source_code(file, line)
+            file = File.open(file)
+            code = ""
+            (line.first - 1).times { file.gets }
+            (line.last - (line.first - 1)).times { code += file.gets }
+          end
       end
     end
   end
