18ddc76805835059d9b71f56d1089a372db1953c
Himanshu Singh <himanshu.singh@venturepact.com>
2016-03-11 11:06:05 +0530

    Done with category generator

--- lib/modules/stats/category_generator.rb
+++ lib/modules/stats/category_generator.rb
@@ -4,32 +4,24 @@ module Stats
 
     def initialize(branch:)
       @branch = branch
-      @stat = {}
+      @stats = {}
     end
 
     def count_issues
       IssueCategory.find_each do |category|
-        @stat[category.name.to_sym] = {
+        @stats[category.name.to_sym] = {
           'issues_count' => 0,
           'files_count' => 0
-        } if !@stat[category.name.to_sym]
-        @stat[category.name.to_sym]['issues_count'] += category.code_issues.where(version: @branch.current_version + 1).count
+        } if !@stats[category.name.to_sym]
+        @stats[category.name.to_sym]['issues_count'] += category.code_issues.where(version: @branch.current_version + 1).count
         files = []
         category.code_issues.where(version: @branch.current_version + 1).each do |issue|
-          # issue.file_list.find_each do |file|
-        #     files << file.id
-        #   end
-        puts "#{issue}\n\n"
-        # end
-        # files.uniq!
-        # @stat[category.name.to_sym]['files_count'] += files.count
+          files << issue.file_list.id
+        end
+        files.uniq!
+        @stats[category.name.to_sym]['files_count'] += files.count
       end
-      puts "---------------------------------------------"
-      puts "Category stats"
-      puts "---------------------------------------------"
-      puts @stat
+      @stats
     end
-
-
   end
 end
