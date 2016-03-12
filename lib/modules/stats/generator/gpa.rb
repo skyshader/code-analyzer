b429f352194a0f3436830f984df49f534924e1b5
Himanshu Singh <himanshu.singh@venturepact.com>
2016-03-12 16:59:46 +0530

    Code for gpa

---
+++ lib/modules/stats/generator/gpa.rb
@@ -0,0 +1,57 @@
+module Stats
+  module Generator
+
+    class GPA
+
+      def initialize(branch:)
+        @branch = branch
+      end
+
+      def self.grade_files(branch:)
+        new(branch: branch).process_files
+      end
+
+      def self.grade_repo(branch:)
+        new(branch: branch).process_repo
+      end
+
+
+      def process_files
+        FileList.get_files_to_process(@branch).each do |file|
+          issue_ranges = []
+          file.code_issues.where(version: @branch.current_version + 1).each do |issue|
+            current_range = issue[:begin_line]..issue[:end_line]
+            issue_ranges << current_range if issue_ranges.size == 0
+            issue_ranges = replace_dup_ranges issue_ranges, current_range
+            puts issue_ranges
+          end
+        end
+      end
+
+
+      def process_repo
+      end
+
+
+      private
+      ##---------------------------------------------
+      #
+      # Helper methods starts here
+      #
+      ##---------------------------------------------
+
+      def replace_dup_ranges ranges, current
+        ranges.each_with_index do |range, index|
+          if range.overlaps?(current)
+            ranges[index] = [range.first, current.first].min..[range.last, current.last].max
+          else
+            ranges << current
+          end
+        end
+        ranges
+      end
+
+    end
+
+  end
+end
--- lib/modules/stats/process.rb
+++ lib/modules/stats/process.rb
@@ -10,13 +10,14 @@ module Stats
     def run
       category_stats = Stats::Generator::Category.new(branch: @branch).generate
       language_stats = Stats::Generator::Language.new(branch: @branch).generate
-      
+
       CategoryStat.transaction do
         ActiveRecord::Base.connection_pool.with_connection do
           @branch.category_stats.create(category_stats)
           @branch.language_stats.create(language_stats)
         end
       end
+      Stats::Generator::GPA.grade_files(branch: @branch)
     end
 
   end
