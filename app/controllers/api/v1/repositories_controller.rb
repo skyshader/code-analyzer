5bc06030cececc47e63c74d3d5d8532c0526b070
Shaan <shardendu.kumar@venturepact.com>
2016-04-25 17:45:24 +0530

    multiple repository with each serializer

--- app/controllers/api/v1/repositories_controller.rb
+++ app/controllers/api/v1/repositories_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::RepositoriesController < Api::V1::BaseController
   def all_by_project
     repositories = Repository.where(project_id: params[:project_id])
     raise ActiveRecord::RecordNotFound unless repositories
-    render json: repositories, serializer: RepositorySerializer, include: [
+    render json: repositories, each_serializer: RepositorySerializer, include: [
       'branches',
       'default_branch',
       'branches.current_request',
